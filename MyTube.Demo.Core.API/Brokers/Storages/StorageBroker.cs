f957f6d8add96c82afd9b83ab003b990b36f7f3f
Kroos <hamroliyevahmadjon1909@gmail.com>
2024-05-13 21:15:25 +0500

    DATA: Migrate VideoMetada

---
+++ MyTube.Demo.Core.API/Brokers/Storages/StorageBroker.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using STX.EFxceptions.SqlServer;
+
+namespace MyTube.Demo.Core.API.Brokers.Storages
+{
+    public partial class StorageBroker : EFxceptionsContext
+    {
+        private readonly IConfiguration configuration;
+
+        public StorageBroker(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+            this.Database.Migrate();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            string connectionString = this.configuration.GetConnectionString("DefaultConnection");
+            optionsBuilder.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
+            optionsBuilder.UseSqlServer(connectionString);
+        }
+    }
+}
