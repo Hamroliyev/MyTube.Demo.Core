1a0b255a0914537ce56629c3dd847a61f12d4786
Kroos <hamroliyevahmadjon1909@gmail.com>
2024-05-14 20:55:36 +0500

    CODE RUB: Implement StorageBroker.VideoMetadatas

---
+++ MyTube.Demo.Core.API/Brokers/Storages/IStorageBroker.VideoMetadatas.cs
@@ -0,0 +1,16 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using System;
+using MyTube.Demo.Core.API.Models.Metadatas;
+
+namespace MyTube.Demo.Core.API.Brokers.Storages
+{
+    public partial interface IStorageBroker
+    {
+        ValueTask<VideoMetadata> InsertVideoMetadataAsync(VideoMetadata videoMetadata);
+        IQueryable<VideoMetadata> SelectAllVideoMetadatas();
+        ValueTask<VideoMetadata> SelectVideoMetadataByIdAsync(Guid videoMetadataId);
+        ValueTask<VideoMetadata> UpdateVideoMetadataAsync(VideoMetadata videoMetadata);
+        ValueTask<VideoMetadata> DeleteVideoMetadataAsync(VideoMetadata videoMetadata);
+    }
+}
