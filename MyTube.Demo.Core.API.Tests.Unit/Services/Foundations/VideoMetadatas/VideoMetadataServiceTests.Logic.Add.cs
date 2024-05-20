90168742176324a2c9d4b504bb8c340c24b7a845
Kroos <hamroliyevahmadjon1909@gmail.com>
2024-05-20 20:54:32 +0500

    CODERUB: Refactor Code.

---
+++ MyTube.Demo.Core.API.Tests.Unit/Services/Foundations/VideoMetadatas/VideoMetadataServiceTests.Logic.Add.cs
@@ -0,0 +1,31 @@
+﻿using Moq;
+using MyTube.Demo.Core.API.Models.Metadatas;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyTube.Demo.Core.API.Tests.Unit.Services.Foundations.VideoMetadatas
+{
+    public partial class VideoMetadataServiceTests
+    {
+        [Fact]
+        public async Task ShouldAddVideoMetadataAsync()
+        {
+            //given
+            VideoMetadata randomVideoMetadata = CreateRandomVideoMetadata();
+            VideoMetadata inputVideoMetadata = randomVideoMetadata;
+            VideoMetadata persistedVideoMetadata = inputVideoMetadata;
+            VideoMetadata expectedVideoMetadata = persistedVideoMetadata.DeepClone();
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.InsertVideoMetadataAsync(inputVideoMetadata))
+                    .ReturnsAsync(persistedVideoMetadata);
+                        
+            //when
+
+            //then
+        }
+    }
+}
---
+++ MyTube.Demo.Core.API.Tests.Unit/Services/Foundations/VideoMetadatas/VideoMetadataServiceTests.cs
@@ -0,0 +1,43 @@
+﻿using Moq;
+using MyTube.Demo.Core.API.Brokers.Storages;
+using MyTube.Demo.Core.API.Models.Metadatas;
+using MyTube.Demo.Core.API.Services.VideoMetadatas;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tynamix.ObjectFiller;
+
+namespace MyTube.Demo.Core.API.Tests.Unit.Services.Foundations.VideoMetadatas
+{
+    public partial class VideoMetadataServiceTests
+    {
+        private readonly Mock<IStorageBroker> storageBrokerMock;
+        private readonly IVideoMetadataService videoMetadataService;
+
+        public VideoMetadataServiceTests()
+        {
+            this.storageBrokerMock = new Mock<IStorageBroker>();
+
+            this.videoMetadataService = new VideoMetadataService(
+                storageBroker: this.storageBrokerMock.Object);
+        }
+
+        private static DateTimeOffset GetRandomDateTime() =>
+            new DateTimeRange(earliestDate: DateTime.UnixEpoch).GetValue();
+
+        private static Filler<VideoMetadata> CreateVideoMetadataFiller()
+        {
+            var filler = new Filler<VideoMetadata>();
+
+            filler.Setup()
+                .OnType<DateTimeOffset>().Use(GetRandomDateTime());
+
+            return filler;
+        }
+
+        private static VideoMetadata CreateRandomVideoMetadata() =>
+            CreateVideoMetadataFiller().Create();
+    }
+}
